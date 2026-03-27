510d1d446487689f02d06bf9932886f41db1c4ae
Your Name <you@example.com>
2026-03-27 11:02:23 +0100

    version intiale du fichier

---
+++ astronautes.py
@@ -0,0 +1,10 @@
+import requests
+
+
+url= "http://api.open-notify.org/astros.json"
+res = requests.get(url)
+
+#[print(f"{elm["name"]}\n") for elm in res.json()["people"]]
+
+l = [elm["name"] for elm in res.json()["people"]]
+print(l)
